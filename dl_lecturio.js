663858fb32522e7390951dba304a2c4ba41a39fa
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-10-17 16:07:13 +0200

    lecturio: comment, remove unnecessary filename processing

--- dl_lecturio.js
+++ dl_lecturio.js
@@ -1,8 +1,16 @@
-export function script_lecturio() {
-  const replaceAll = (target, pattern, replacement) => {
-    return target.replace(new RegExp(pattern, 'g'), replacement);
-  };
+/*
+What's going on here?
 
+We read the url of the video file from the `window.lecturioJwplayer` object.
+However, chrome extension content scripts can't access variables created by another webpage.
+To work around that, we create a `<script>` element (which gets esecuted in the
+full isolate of the webpage and therefore can access the `lecturioJwplayer` object).
+The script then uses `window.postMessage` to relay the object back to the content script
+(this works because the content script and the `<script>` element share the same `window`).
+We extract the data we're looking for and use the `chrome.runtime.Port` API to send that back to the extension
+*/
+
+export function script_lecturio() {
   const filterHdUrl = urls => {
     for (const url of urls) {
       if (url.label === 'HD') return url.file;
@@ -14,11 +22,9 @@ export function script_lecturio() {
     const courseName = document.getElementsByClassName('breadcrumb_item bc_basic_course')[0].children[0].text.trim();
     const lectureName = document.getElementsByClassName('breadcrumb_item bc_lecture')[0].textContent.trim();
 
-    const title = replaceAll(`${lectureName} - ${courseName}`, '/', '-');
-
     const port = chrome.runtime.connect(chrome.runtime.id);
     port.postMessage({
-      title,
+      title: `${lectureName} - ${courseName}`,
       videoUrls: [filterHdUrl(event.data)]
     });
   }, false);
