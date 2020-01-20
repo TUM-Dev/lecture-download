d011addcf9ea05d9209acc4ee139eb599445cab7
Tim Schumacher <timschumi@gmx.de>
2020-01-20 11:02:30 +0100

    Do not use Chrome's declarativeContent system
    Firefox doesn't support that yet.

--- background.js
+++ background.js
@@ -1,20 +1,7 @@
-
-const pageStateMatcherForHost = host => {
-  return new chrome.declarativeContent.PageStateMatcher({
-    pageUrl: { hostEquals: host }
-  });
-};
-
-// Whenever the extension is installed or updated, we replace all old rules w/ a new one,
-// which enables the page action on all matching hosts
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-          'streams.tum.de', 'www.lecturio.de'
-      ].map(pageStateMatcherForHost),
-      actions: [ new chrome.declarativeContent.ShowPageAction() ]
-    }]);
-  });
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (tab.url.match(/streams.tum.de|www.lecturio.de/)) {
+        chrome.pageAction.show(tabId);
+    } else {
+        chrome.pageAction.hide(tabId);
+    }
 });
