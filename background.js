f3d1639263f00d4524cbee0f6d691a84c785fe85
Lukas Kollmer <me@lukaskollmer.de>
2020-02-09 14:57:48 +0100

    fix indent

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (tab.url.match(/streams.tum.de|www.lecturio.de/)) {
-        chrome.pageAction.show(tabId);
-    } else {
-        chrome.pageAction.hide(tabId);
-    }
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.url.match(/streams.tum.de|www.lecturio.de/)) {
+    chrome.pageAction.show(tabId);
+  } else {
+    chrome.pageAction.hide(tabId);
+  }
 });
